08937d3158d5629c63affe74cbb342b5b75d9b83
Jas1052 <jliu1052@gmail.com>
2020-12-02 23:20:22 -0500

    day 1 jason

---
+++ Jason/day1/day1.py
@@ -0,0 +1,16 @@
+f = open("input.txt", "r")
+target = 2020
+nums = []
+with open('input.txt') as my_file:
+    for num in my_file:
+        nums.append(int(num))
+
+inverse = []
+for num in nums:
+    inverse.append(target - num)
+
+for num in nums:
+    if inverse.__contains__(num):
+        print(num * (target - num))
+
+# 987339
